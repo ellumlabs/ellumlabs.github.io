0fc8ef86f37f94df175e52b2046a11d17ade6335
Konstantin Polyashenko <konstantinpolyashenko@Konstantins-MBP.attlocal.net>
2021-01-31 19:26:40 -0600

    updated careers email

--- js/secondary.js
+++ js/secondary.js
@@ -5,6 +5,7 @@ const careerFormLName = document.querySelector('#career_form-ln');
 const careerFormEmail = document.querySelector('#career_form-email');
 
 submitButton.onclick = function() {
+    alert("Thank you for signing up! We will keep you updated on future job posts.");
     careerFormFName.value = "";
     careerFormLName.value = "";
     careerFormEmail.value = "";
